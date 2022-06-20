3baa6355dae3071b1166c77c89a99cd6c674b94d
Matt Berti <mat.berti@gmail.com>
2022-06-20 12:15:12 +0800

    Added reverse array exercise

---
+++ assignments/warmups/reverse-array.js
@@ -0,0 +1,13 @@
+const joinCode = 'GK8UDQ'
+
+function reverseArray(array) {
+  // your code here
+  // You must write your algorithm WITHOUT using JavaScript's
+  // `reverse` method or CODE.ORG's `append` method.
+}
+
+// Tests
+console.log(
+  'reverseArray([1, 2, 3, 4, 5]) is [5, 4, 3, 2, 1]: ',
+  reverseArray([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]
+)
